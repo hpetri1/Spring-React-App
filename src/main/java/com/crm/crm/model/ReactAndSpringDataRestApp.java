de4401bc7deef05cca7c7c2196d01cad14bfe012
hpetri1 <39070807+hpetri1@users.noreply.github.com>
2020-03-11 20:10:05 -0600

    Missed to save files

---
+++ src/main/java/com/crm/crm/model/ReactAndSpringDataRestApp.java
@@ -0,0 +1,12 @@
+//This file will allow to run the Server
+package com.crm.crm.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringDataRestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactAndSpringDataRestApp.class, args);
+    }
+}
