9ee6454cfe5a016e8d1831e3f1027b1f8b070d49
hpetri1 <39070807+hpetri1@users.noreply.github.com>
2020-03-12 00:54:07 -0600

    Update App.js, save index.html changes
    1. Saved unsaved added links in index.html file.
    2. Undated App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Contacts />
-    </div>
+    <div className="container-fluid">
+      <nav>
+        <div className="nav-wrapper center-align">
+          <a href="/" className="brand-logo">Contacts</a>
+        </div>
+      </nav>
+      <div className="row">
+        <Contacts />
+      </div>
+    </div>    
   );
 }
 
